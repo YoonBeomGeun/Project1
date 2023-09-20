34a0ade57f08864c23cc5233c3c4105a6e75e4c6
YoonBeomGeun <yunbumkun36@naver.com>
2023-09-20 17:26:31 +0900

    커밋 3 복원

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ def add_func(n1, n2):
 def sub_func(n1, n2):
     return n1 - n2
 
+def gob_func(n1, n2):
+    return n1 * n2
+
+def div_func(n1, n2):
+    return n1 / n2
+
 ## 전역 변수부
 num1 = 100
 num2 = 200
@@ -16,3 +22,9 @@ print(num1, "+", num2, "=", res)
 
 res = sub_func(num1, num2)
 print(num1, "-", num2, "=", res)
+
+res = gob_func(num1, num2)
+print(num1, "*", num2, "=", res)
+
+res = div_func(num1, num2)
+print(num1, "/", num2, "=", res)
